bdbe6cc46d7a7bcf25dd236590dc52e514d3c0ed
dima-pi <avida.d3@gmail.com>
2017-12-29 22:27:28 +0200

    Add ssh keys configuration

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,13 @@ $PACKAGE_MGR $PACKAGE_LIST
 sudo pip3 install $PIP
 ;;
 "config")
+mkdir -p ~/.ssh
+openssl aes-256-cbc -d -in keys.tgz.aes256cbc.enc -out ~/.ssh/keys.tgz
+pushd ~/.ssh
+tar -xzf keys.tgz
+cat id_rsa.pub >> authorized_keys
+chmod 600 /home/pi/.ssh/id_rsa
+popd
 ln -s ~/.vim/.vimrc ~/.vimrc
 git clone https://github.com/tpope/vim-pathogen.git
 mv vim-pathogen/autoload ./
