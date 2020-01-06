2c144c30e55ac039794c83c864bfedba5113dcfc
Dmytro <avida.d3@gmail.com>
2020-01-06 18:28:12 +0000

    Fix initial script not cloning repo

--- initial_deploy.sh
+++ initial_deploy.sh
@@ -1,8 +1,8 @@
 which git > /dev/null || {
    sudo apt update
    sudo apt install git
-   git clone https://github.com/avida/.vim.git
-   cd .vim
-   git remote rm origin
-   git remote add origin git@github.com:avida/.vim.git
 }
+git clone https://github.com/avida/.vim.git
+cd .vim
+git remote rm origin
+git remote add origin git@github.com:avida/.vim.git
